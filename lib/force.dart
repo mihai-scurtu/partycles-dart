ed8bab4d1c93056cc22eb8f1e5c9e3bd9998d622
Mihai Scurtu <mscurtu90@gmail.com>
2014-09-19 14:51:10 +0300

    Add random force constructor

--- lib/force.dart
+++ lib/force.dart
@@ -9,6 +9,15 @@ class Force {
   num get abs => sqrt(this.x * this.x + this.y * this.y);
   
   Force(this.x, this.y);
+  Force.random(num degree, {Random rng}) {
+    if(rng == null) {
+      rng = new Random();
+    }
+    
+    this.x = 2 * rng.nextDouble() * degree - degree;
+    this.y = 2 * rng.nextDouble() * degree - degree;
+  }
+
   
   Force operator+(Force f) => new Force(f.x + this.x, f.y + this.y);
   Force operator*(num k) => new Force(this.x * k, this.y * k);
--- test/force_test.dart
+++ test/force_test.dart
@@ -1,5 +1,7 @@
 library force_test;
 
+import 'dart:math';
+
 import 'package:partycles/force.dart';
 import 'package:unittest/unittest.dart';
 
@@ -12,16 +14,24 @@ void main() {
     expect(f + g, new Force(8, 10));
   });
   
+  test('Random', (){
+    Force f = new Force.random(5);
+    
+    expect(f.x > -5 && f.x < 5, true);
+    expect(f.y > -5 && f.y < 5, true);
+    expect(f.abs < 5 * SQRT2, true);
+  });
+  
   test('Absolute value', (){
     Force f;
     
     f = new Force(0, 1);
-    expect(f.abs(), 1);
+    expect(f.abs, 1);
     
     f = new Force(1, 1);
-    expect(f.abs(), closeTo(1.41, 0.01));
+    expect(f.abs, closeTo(1.41, 0.01));
     
     f = new Force(-1, -1);
-    expect(f.abs(), closeTo(1.41, 0.01));
+    expect(f.abs, closeTo(1.41, 0.01));
   });
 }
