2fd2ebbea1d44f7fe4720b89bdc6a718cce8ac97
Mihai Scurtu <mscurtu90@gmail.com>
2014-09-19 14:50:56 +0300

    Move absolute value to dart "getter"

--- lib/force.dart
+++ lib/force.dart
@@ -5,10 +5,10 @@ import 'dart:math';
 class Force {
   num x = 0;
   num y = 0;
+
+  num get abs => sqrt(this.x * this.x + this.y * this.y);
   
   Force(this.x, this.y);
-  
-  num abs() => sqrt(this.x * this.x + this.y * this.y);
   
   Force operator+(Force f) => new Force(f.x + this.x, f.y + this.y);
   Force operator*(num k) => new Force(this.x * k, this.y * k);
