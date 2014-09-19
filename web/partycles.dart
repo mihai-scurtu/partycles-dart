4cdc0602f0b7ebb982e50afffb270c3bec056260
Mihai Scurtu <mscurtu90@gmail.com>
2014-09-19 15:01:32 +0300

    Add random forces

--- web/partycles.dart
+++ web/partycles.dart
@@ -3,12 +3,17 @@ library partycles;
 import 'dart:html';
 
 import 'package:partycles/particle_engine.dart';
+import 'package:partycles/particle.dart';
+import 'package:partycles/force.dart';
 
 void main() {
   ParticleEngine engine = new ParticleEngine(document.querySelector('canvas#main'));
   
   engine.drawCallback = () {
-    engine.particles.add(engine.randomParticle());
+    Particle p = engine.randomParticle();
+    p.force = new Force.random(0.2);
+    engine.particles.add(p);
+    
     engine.bufferText('Particles: ${engine.particles.length}\n');
   };
   
