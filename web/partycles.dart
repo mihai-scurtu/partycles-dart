86f76eda4a330c2454f59cfc695f7607b62a280e
Mihai Scurtu <mscurtu90@gmail.com>
2014-09-19 14:45:14 +0300

    Benchmark particle count

--- web/partycles.dart
+++ web/partycles.dart
@@ -7,10 +7,10 @@ import 'package:partycles/particle_engine.dart';
 void main() {
   ParticleEngine engine = new ParticleEngine(document.querySelector('canvas#main'));
   
-  engine.particles.add(engine.randomParticle());
-  engine.particles.add(engine.randomParticle());
-  engine.particles.add(engine.randomParticle());
-  engine.particles.add(engine.randomParticle());
-//  engine.bufferText('Hello World\n!!');
+  engine.drawCallback = () {
+    engine.particles.add(engine.randomParticle());
+    engine.bufferText('Particles: ${engine.particles.length}\n');
+  };
+  
   engine.loop();
 }
